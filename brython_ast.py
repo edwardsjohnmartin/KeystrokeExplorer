7693707d56656bbe217e66cc4f5883e6bde0b452
Steven <steven.scott@aggiemail.usu.edu>
2022-12-14 21:22:53 -0700

    Added col offset and line number to brython json

--- brython_ast.py
+++ brython_ast.py
@@ -2,18 +2,24 @@ import ast
 import json
 from browser import window
 
-def get_display_name(ast_node):
+
+def get_ast_node_type(ast_node):
     return type(ast_node).__name__
 
 
 def get_node_data(ast_node):
     result = {
-        "ast_node_type": get_display_name(ast_node),
+        "ast_node_type": get_ast_node_type(ast_node),
         "children": [get_node_data(child) for child in ast.iter_child_nodes(ast_node)]
     }
+    if hasattr(ast_node, "col_offset"):
+        result["col_offset"] = getattr(ast_node, "col_offset")
+    if hasattr(ast_node, "lineno"):
+        result["lineno"] = getattr(ast_node, "lineno")
+
     for field in ast_node._fields:
         assert field != "children"
-        assert field != "ast_node_type"
+        assert field != "display_name"
         retrieved_attribute = ast_node.__getattribute__(field)
         if type(retrieved_attribute) == str:
             result[field] = retrieved_attribute
