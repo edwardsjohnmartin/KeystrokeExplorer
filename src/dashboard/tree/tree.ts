695a0257132826d5a43bd6ff5a9e061656bdb81a
John Edwards <edwards@Johns-MacBook-Pro-3.local>
2023-04-29 13:32:28 -0600

    rudimentary correspondence visualization

--- src/dashboard/tree/tree.ts
+++ src/dashboard/tree/tree.ts
@@ -3,14 +3,20 @@ import { Data } from '../../data';
 import * as d3 from "d3";
 import { inject } from 'aurelia';
 import { watch } from '@aurelia/runtime-html';
+import { isFunction } from 'util';
+import { AstNode } from '../../ast';
 
 
 @inject(Data)
 export class Tree {
     data: Data;
+    selectedParent: number;
+    parents: Set<number>;
 
     constructor(data: Data) {
         this.data = data;
+        this.selectedParent = -1;
+        this.parents = new Set<number>();
     }
 
     attached() {
@@ -24,12 +30,12 @@ export class Tree {
     @watch("data.playback")
     buildTree() {
         const ast = this.data.precompiledAsts[this.data.playback];
-        if (ast === null || ast === undefined) return;
-
-        // console.log(ast);
+        // if (ast === null || ast === undefined) return;
 
         // clear old html out
         d3.select("g#tree").selectAll("*").remove();
+
+        if (ast === null || ast === undefined) return;
 
         const size = d3.select("svg#tree").node().getBoundingClientRect();
         const treeRoot = d3.hierarchy(ast);
@@ -57,7 +63,25 @@ export class Tree {
             .attr("text-anchor", d => d.children === undefined ? "start" : "end")
             .attr("dx", d => d.children === undefined ? 10 : -10)
             .attr("dy", d => d.children === undefined ? 5 : -10)
-            ;
+            .on("mouseover", (event: MouseEvent, d) => {
+                let node: AstNode = d.data;
+                console.log(node.name, 'cid='+node.cid, 'eventNum='+node.eventNum, 'tparent='+node.tparent, 'edits='+node.num_edits);
+                this.selectedParent = node.tparent;
+                // Find the root parent
+                let n: AstNode = node;
+                this.parents.clear();
+                this.parents.add(n.cid);
+                while (n.tparent !== undefined) {
+                    n = this.data.cid2node[n.tparent];
+                    this.parents.add(n.cid);
+                }
+                console.log('Origin node:', n);
+            })
+            // .on("mouseout", function() {
+            //     // // Remove the info text on mouse out.
+            //     // d3.select(this).select('text.info').remove()
+            //   });            
+        ;
 
         d3.select("g#tree")
             .selectAll("path")
@@ -74,8 +98,11 @@ export class Tree {
             .append("g")
             .attr("transform", d => `translate(${d.y}, ${d.x})`)
             .append("circle")
-            .attr("r", 3)
-            .attr("fill", "#364e74")
+            .attr("r", d => this.parents.has(d.data.cid) ? 6 : 3)
+            .attr("fill", d => {
+                
+                return this.parents.has(d.data.cid) ? "#ff0000" : "#364e74";
+            })
             ;
     }
 }
